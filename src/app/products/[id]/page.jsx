efb60000ff89375ad3573ba94016a778cde01ad4
astroboy20 <tolulopeakinkunmi7@gmail.com>
2024-07-26 17:43:58 +0100

    feat: worked on the single product page

--- src/app/products/[id]/page.jsx
+++ src/app/products/[id]/page.jsx
@@ -1,35 +1,11 @@
+"use client";
 
-// "use client"
-// import { BASE_URL, useFetchItems } from "@/hooks/useFetchItems";
-// import data from "@/providers/fakeJsonData";
-// import React from "react";
-// import data from "@/providers/fakeJsonData";
-// import { notFound } from "next/navigation";
-// import React from "react";
-
-// const Page = ({ params }) => {
-//   const { id } = params;
-//   const getSeller = data.find((product) => product._id === 1);
-//   const { data: getSingleProduct } = useFetchItems({
-//     url: `${BASE_URL}/${id}`,
-//   });
-  
-
-//   return (
-//     <div>
-//       <p>{id}</p>
-//     </div>
-//   );
-// };
-
-// export default Page;
-
-
-"use client"
+import { Footer } from "@/components/footer";
+import { Promotion } from "@/components/promotion";
+import { OtherProducts } from "@/container/singleProduct/otherProducts";
 import { SingleProduct } from "@/container/singleProduct/singleProduct";
 import { BASE_URL, useFetchItems } from "@/hooks/useFetchItems";
 import data from "@/providers/fakeJsonData";
-import { notFound } from "next/navigation";
 import React from "react";
 
 const Page = ({ params }) => {
@@ -38,18 +14,15 @@ const Page = ({ params }) => {
   const { data: getSingleProduct } = useFetchItems({
     url: `${BASE_URL}/${id}`,
   });
-//   if (!getSingleProduct) return notFound();
-console.log(getSingleProduct?.rating?.rate)
-console.log(getSingleProduct?.image)
 
   return (
     <main>
-        {/* {id} */}
       <SingleProduct seller={getSeller} getSingleProduct={getSingleProduct} />
+      <OtherProducts />
+      <Promotion />
+      <Footer />
     </main>
   );
 };
 
 export default Page;
-
-
---
+++ src/container/singleProduct/otherProducts.jsx
@@ -0,0 +1,63 @@
+import { Fav } from "@/assets";
+import Link from "next/link";
+import { BASE_URL, useFetchItems } from "@/hooks/useFetchItems";
+import { StarRating } from "@/components/rating";
+import { AddToCart } from "@/components/addToCart";
+
+const OtherProducts = () => {
+  const { data: newData } = useFetchItems({ url: `${BASE_URL}?limit=4` });
+  console.log("hey", newData);
+  return (
+    <div className="my-10 lg:py-3 lg:px-0">
+      <div>
+        <p className="text-[24px] pl-3 mb-5 leading-[29.26px] font-semibold sm:text-[40px] sm:leading-[48.76px]">
+          Other Products
+        </p>
+        <div className="p-3 sm:py-3 sm:px-0 grid grid-cols-2 gap-[15px] md:grid-cols-2 lg:grid-cols-4 ">
+          {newData?.map((singleProduct) => {
+            return (
+              <div
+                key={singleProduct.id}
+                className="pb-3 max-w-[180px] cursor-pointer shadow-md bg-[#F2F3F4] sm:max-w-[295px]"
+              >
+                <Link href={`/products/${singleProduct.id}`}>
+                  <img
+                    className="h-[175px] w-[180px] object-contain bg-white sm:h-[290px] sm:w-[295px]"
+                    src={singleProduct.image}
+                    alt=""
+                  />
+                </Link>
+                <div className="px-3 pt-[.5em] flex flex-col gap-[.5em]">
+                  <div className="flex  items-center justify-between">
+                    <Link href={`/products/${singleProduct.id}`}>
+                      <p className="capitalize font-semibold text-[10px] w-[98px] h-[12px] leading-[12.19px] sm:w-[215px] sm:h-[22px] sm:text-[18px] sm:leading-[21.94px] truncate">
+                        {singleProduct.title}
+                      </p>
+                    </Link>
+                    <Fav />
+                  </div>
+                  <p className="text-[#888282] text-[8px] leading-[9.75px] w-[70px] h-[10px] sm:w-[105px] sm:h-[20px] sm:text-[16px] sm:leading-[19.5px] truncate">
+                    &#8358;{singleProduct.price}
+                  </p>
+                  <StarRating
+                    rating={singleProduct.rating.rate || 5}
+                    width={10}
+                    height={10}
+                  />
+                  <AddToCart
+                    id={singleProduct.id}
+                    className="bg-black w-[64px] h-[20px] text-[8px] leading-[9.75px] sm:w-[132px] sm:h-[36px] sm:text-[14px] sm:leading-[17.07px] text-white rounded-sm border-none"
+                  >
+                    Add to cart
+                  </AddToCart>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export { OtherProducts };
