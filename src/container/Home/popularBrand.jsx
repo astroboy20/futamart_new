ce7f61de250e1dbff39aeae5351489bf8628de81
CodeThiefX <timiwade97@gmail.com>
2024-09-05 09:54:00 +0100

    feat: home page edit, font fix and minor responsiveness adjustment

--- src/container/Home/popularBrand.jsx
+++ src/container/Home/popularBrand.jsx
@@ -2,19 +2,20 @@ import Image from "next/image";
 
 const PopularBrand = () => {
   return (
-    <div className="pt-10 flex flex-col gap-4">
+    <div className="pt-10 flex flex-col gap-6">
       <header className="text-[20px] lg:text-[35px] font-[600]">
         Popular Brands
       </header>
       <div className="flex w-full overflow-x-scroll no-scrollbar gap-[20px]">
         <div className="flex justify-between p-[3%] flex-shrink-0 bg-[#DBDBDB] rounded-[4px] h-fit w-[300px] lg:overflow-y-hidden lg:w-[720px] lg:p-[1.5%] lg:h-[250px]">
           <div className="flex flex-col  gap-[15px] h-full">
-            <div className="flex flex-col gap-[5px]">
-              <h1 className="text-[18px] lg:text-[24px] font-[600]">La_Spag</h1>
-              <p className="text-[8px] mr-5 lg:text-[16px] font-[400]">
+            <div className="flex flex-col gap-[5px] h-full justify-between">
+              <h1 className="text-[18px] lg:text-[36px] font-[600]">La_Spag</h1>
+              <p className="text-[10px] mr-5 lg:text-[16px] font-[400]">
                 Forem ipsum dolor sit amet, consectetur adipiscing elit. Nunc
                 vulputate libero et velit interdum.
               </p>
+              <div></div>
             </div>
             <button className="mt-auto w-fit text-[8px] px-[20px] lg:text-[16px] font-[400] text-[#FFFFFF] lg:px-[40px] py-[8px] rounded-[4px] bg-black">
               View Brand
@@ -32,12 +33,13 @@ const PopularBrand = () => {
         </div>
         <div className="flex justify-between p-[3%] flex-shrink-0 bg-[#DBDBDB] rounded-[4px] h-fit w-[300px] overflow-hidden lg:w-[720px] lg:p-[1.5%]  lg:h-[250px] ">
           <div className="flex flex-col  gap-[20px] h-full">
-            <div className="flex flex-col gap-[5px]">
-              <h1 className="text-[18px] lg:text-[24px] font-[600]">La_Spag</h1>
-              <p className="text-[8px] mr-5 lg:text-[16px] font-[400]">
+            <div className="flex flex-col gap-[5px] h-full justify-between">
+              <h1 className="text-[18px] lg:text-[36px] font-[600]">La_Spag</h1>
+              <p className="text-[10px] mr-5 lg:text-[16px] font-[400]">
                 Forem ipsum dolor sit amet, consectetur adipiscing elit. Nunc
                 vulputate libero et velit interdum.
               </p>
+              <div></div>
             </div>
             <button className="mt-auto w-fit text-[8px] px-[20px] lg:text-[16px] font-[400] text-[#FFFFFF] lg:px-[40px] py-[8px] rounded-[4px] bg-black">
               View Brand
