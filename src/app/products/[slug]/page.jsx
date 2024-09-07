9b0b71a74dc979694247fa681b82cdc06fbed977
CodeThiefX <timiwade97@gmail.com>
2024-09-07 20:15:24 +0100

    feat: improved view product preloader, font-adjustment for other products under products

--- src/app/products/[slug]/page.jsx
+++ src/app/products/[slug]/page.jsx
@@ -7,6 +7,8 @@ import { OtherProducts } from "@/container/singleProduct/otherProducts";
 import { SingleProduct } from "@/container/singleProduct/singleProduct";
 import { BASE_URL, useFetchItems } from "@/hooks/useFetchItems";
 import React from "react";
+import { Typewriter } from "react-simple-typewriter";
+import { Logo_Black } from "@/assets";
 
 const Page = ({ params }) => {
   const { slug } = params;
@@ -18,19 +20,39 @@ const Page = ({ params }) => {
     url: `${BASE_URL}/product/${slug}`,
   });
 
-  if (isLoading) return <div>Loading</div>;
+  if (isLoading)
+    return (
+      <div className="w-full h-[100dvh] flex items-center justify-center">
+        <div className="flex flex-col text-center">
+          <Logo_Black />
+          <h1 className="text-[32px] font-[600]">
+            {" "}
+            <Typewriter
+              words={["futamart"]}
+              loop={5}
+              cursor
+              cursorStyle="_"
+              typeSpeed={80}
+              deleteSpeed={50}
+              delaySpeed={1000}
+            />
+            {/* futamart */}
+          </h1>
+        </div>
+      </div>
+    );
   if (error) return <div>{error.message}</div>;
 
   return (
     <main>
       <Header />
       <div className="px-[6%]">
-      <SingleProduct getSingleProduct={getSingleProduct} />
-      <OtherProducts
-        relatedProducts={getSingleProduct?.data?.relatedProducts}
-      />
+        <SingleProduct getSingleProduct={getSingleProduct} />
+        <OtherProducts
+          relatedProducts={getSingleProduct?.data?.relatedProducts}
+        />
       </div>
-      
+
       <Promotion />
       <Footer />
     </main>
