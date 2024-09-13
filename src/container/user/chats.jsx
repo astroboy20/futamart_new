0d8cf0efa9ff600843f8a095c7d5453f5eea4f2e
astroboy20 <tolulopeakinkunmi7@gmail.com>
2024-09-13 21:14:19 +0100

    updated version

--- src/container/user/chats.jsx
+++ src/container/user/chats.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { FiSend } from "react-icons/fi";
 import axios from "axios";
 import Cookies from "js-cookie";
@@ -6,51 +6,75 @@ import { useFetchItems } from "@/hooks/useFetchItems";
 import { NotificationIconX } from "@/assets";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useTimestamp } from "@/hooks/useTimeStamp";
+import { IoIosArrowBack } from "react-icons/io";
+import { useMediaQuery } from "@/hooks/useMediaQuery";
 
 const Chats = ({ id }) => {
-  const { data: messages, mutate: updateMessages } = useFetchItems({
-    url: `${process.env.NEXT_PUBLIC_API_URL}/chat/${id}`,
-  });
+  const queryClient = useQueryClient();
+  const token = Cookies.get("token");
+  const [message, setMessage] = useState("");
+  const [sending, setSending] = useState(false);
+  const [selectedUser, setSelectedUser] = useState(null);
+  const messagesEndRef = useRef(null);
+  const isDesktop = useMediaQuery("(min-width: 1024px)");
+
   const { data: userData } = useFetchItems({
     url: `${process.env.NEXT_PUBLIC_API_URL}/chats`,
   });
+
   const { data: user } = useFetchItems({
     url: `${process.env.NEXT_PUBLIC_API_URL}/user`,
   });
 
-  const token = Cookies.get("token");
-  const [isChat, setIsChat] = useState(false);
-  const [selectedUser, setSelectedUser] = useState(null);
-  const [message, setMessage] = useState("");
-  const [sending, setSending] = useState(false);
+  const { data: messages } = useFetchItems({
+    url: selectedUser ? `${process.env.NEXT_PUBLIC_API_URL}/chat/${selectedUser._id}` : null,
+    enabled: !!selectedUser,
+  });
 
-  // Assuming userData returns an object with a property '_id' as the current user ID
-  const currentUserId = user?.data?._id;
+  useEffect(() => {
+    if (messagesEndRef.current) {
+      messagesEndRef.current.scrollIntoView({ behavior: "smooth" });
+    }
+  }, [messages]);
 
   const handleClick = (user) => {
     setSelectedUser(user);
-    setIsChat(true);
   };
+
+  const sendMessageMutation = useMutation({
+    mutationFn: async () => {
+      try {
+        const response = await axios.post(
+          `${process.env.NEXT_PUBLIC_API_URL}/chat/${selectedUser._id}`,
+          { message },
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          }
+        );
+        return response.data;
+      } catch (error) {
+        throw new Error("Something went wrong!");
+      }
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: [`${process.env.NEXT_PUBLIC_API_URL}/chat/${selectedUser._id}`],
+        queryKey: [`${process.env.NEXT_PUBLIC_API_URL}/chats`],
+      });
+      setMessage("");
+    },
+  });
 
   const handleSendMessage = async () => {
     if (!message.trim()) return;
 
     setSending(true);
     try {
-      const response = await axios.post(
-        `${process.env.NEXT_PUBLIC_API_URL}/chat/${id}`,
-        { message: message },
-        {
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
-        }
-      );
-
-      if (response.status === 200) {
-        setMessage("");
-        updateMessages();
-      }
+      await sendMessageMutation.mutateAsync();
     } catch (error) {
       console.error("Error sending message:", error);
     } finally {
@@ -61,7 +85,7 @@ const Chats = ({ id }) => {
   return (
     <div className="flex flex-col gap-10 p-[6%]">
       <div className="flex justify-between items-center">
-        <h1 className="text-[24px] font-[600] underline">Chat</h1>
+        <h1 className="text-[24px] font-semibold underline">Chat</h1>
         <div className="flex items-center gap-5">
           <NotificationIconX />
           <Link href="/dashboard/products">
@@ -70,82 +94,100 @@ const Chats = ({ id }) => {
         </div>
       </div>
 
-      <div className="flex flex-col lg:flex-row lg:justify-between w-full">
+      <div className="flex flex-col lg:flex-row lg:justify-between w-full h-full">
         {/* Chat List Section */}
-        <div className="w-full lg:w-[30%]">
-          <div className="flex justify-between items-center text-[18px] font-[500]">
-            <p>All Chats ({userData?.data?.length})</p>
-            <p className="hidden lg:flex">Oldest</p>
-          </div>
+        {(!selectedUser || isDesktop) && (
+          <div className="w-full lg:w-[30%]">
+            <div className="flex justify-between items-center text-[18px] font-medium">
+              <p>
+                All Chats
+                <span className="text-[#51A40A]"> ({userData?.data?.length})</span>
+              </p>
+              <p className="hidden lg:flex">Oldest</p>
+            </div>
 
-          <div>
-            {userData?.data?.map((user) => (
-              <div
-                key={user._id}
-                className="flex justify-between items-center py-4 border-b cursor-pointer hover:bg-gray-100"
-                onClick={() => handleClick(user?.userInfo?.firstname)}
-              >
-                <p>{user?.userInfo?.firstname}</p>
-              </div>
-            ))}
+            <div>
+              {userData?.data?.map((user) => (
+                <div
+                  key={user._id}
+                  className="flex justify-between items-center py-4 border-b cursor-pointer hover:bg-gray-100"
+                  onClick={() => handleClick(user)}
+                >
+                  <div className="flex flex-col gap-2">
+                    <p className="text-[14px] font-[500]">
+                      {user?.userInfo?.firstname} {user?.userInfo?.lastname}
+                    </p>
+                    <p className="text-gray-600 text-[12px] font-[500]">
+                      {user?.lastMessage?.message}
+                    </p>
+                  </div>
+
+                  <p className="text-[#51A40A] text-[10px] font-[600]">
+                    {useTimestamp({ timestamp: user?.lastMessage?.createdAt })}
+                  </p>
+                </div>
+              ))}
+            </div>
           </div>
-        </div>
+        )}
 
         {/* Chat Window Section */}
-        {isChat && (
-          <div className="w-full lg:w-[60%] h-[500px] p-4 rounded-lg flex flex-col justify-between">
-            {/* Chat Header */}
-            <div className="bg-white p-4 rounded-t-lg shadow-md flex justify-between items-center rounded-[16px]">
-              <h2 className="text-[20px] font-bold">
-                {selectedUser?.businessName || "Chat"}
+        {selectedUser && (
+          <div
+            className={`w-full lg:w-[60%] flex flex-col h-[400px] bg-[url('/images/products/chat-bg.png')] bg-cover bg-no-repeat rounded-lg shadow-lg relative ${
+              !isDesktop ? "fixed top-0 left-0 w-full h-full z-50" : ""
+            }`}
+          >
+            <div className="bg-white p-4 m-2 shadow-md sticky top-0 z-10 rounded-t-lg flex justify-between items-center">
+              <h2 className="text-[14px] font-[500] flex gap-2 items-center">
+                {!isDesktop && (
+                  <button onClick={() => setSelectedUser(null)}>
+                    <IoIosArrowBack />
+                  </button>
+                )}
+                {selectedUser?.userInfo?.firstname} {selectedUser?.userInfo?.lastname}
               </h2>
             </div>
 
-            {/* Messages Area */}
-            <div className="flex-grow bg-[url('/images/products/chat-bg.png')] bg-cover bg-no-repeat p-4 overflow-y-auto">
+            <div className="flex-grow overflow-y-auto p-4">
               <div className="flex flex-col gap-4">
-                {messages?.data?.messages?.map((msg) => (
+                {messages?.data?.conversation?.messages?.map((msg) => (
                   <div
                     key={msg._id}
-                    className={`flex ${
-                      msg.senderId === currentUserId
-                        ? "justify-end" // Outgoing messages (right-aligned)
-                        : "justify-start" // Incoming messages (left-aligned)
-                    }`}
+                    className={`flex ${msg?.senderId === user?.data?._id ? "ml-auto" : "mr-auto"}`}
                   >
-                    <div className="flex flex-col w-full">
+                    <div className="flex flex-col max-w-full">
                       <div
-                        className={`p-3 rounded-lg max-w-fit ${
-                          msg.senderId === currentUserId
-                            ? "bg-blue-500 text-white" // Outgoing message style
-                            : "bg-gray-200 text-black" // Incoming message style
+                        className={`p-3 text-[12px] rounded-lg max-w-full ${
+                          msg.senderId === user?.data?._id
+                            ? "bg-black text-white ml-auto"
+                            : "bg-white text-black mr-auto shadow-md border border-gray-200"
                         }`}
                       >
                         <p>{msg.message}</p>
                       </div>
-                      <span className="text-xs text-gray-400">
+                      <span className="text-[10px] font-[500] mt-1">
                         {new Date(msg.createdAt).toLocaleTimeString()}
                       </span>
                     </div>
                   </div>
                 ))}
+
+                <div ref={messagesEndRef} />
               </div>
             </div>
 
-            <div className="bg-white p-4 rounded-b-lg shadow-md flex items-center gap-3">
+            {/* Message Input Section */}
+            <div className="bg-white p-3 shadow-md flex items-center gap-3 sticky bottom-0 z-10">
               <input
                 type="text"
                 value={message}
                 onChange={(e) => setMessage(e.target.value)}
                 placeholder="Type a message..."
-                className="flex-grow border border-gray-300 p-3 rounded-lg focus:outline-none focus:ring focus:ring-purple-300"
+                className="flex-grow border border-gray-300 p-3 rounded-lg focus:outline-none focus:ring focus:ring-black"
                 disabled={sending}
               />
-              <button
-                className="text-purple-500"
-                onClick={handleSendMessage}
-                disabled={sending}
-              >
+              <button onClick={handleSendMessage} disabled={sending}>
                 <FiSend size={24} />
               </button>
             </div>
