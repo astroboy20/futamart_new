5ff8d97feff301750a938e814fbbc6b4b063efa2
astroboy20 <tolulopeakinkunmi7@gmail.com>
2024-09-20 09:38:33 +0100

    feat:work on the seller profile page

--- src/container/dashboard/settings/profile.jsx
+++ src/container/dashboard/settings/profile.jsx
@@ -13,6 +13,12 @@ const Profile = ({ setSelected }) => {
         </button>
         Profile
       </div>
+      <div className="relative w-full">
+        <div className="w-auto h-[180px] rounded bg-[#F2F3F4] "> </div>
+        <div className="w-[140px] h-[140px] rounded-full bg-[#fff] absolute bottom-[-30%] left-1/2 transform -translate-x-1/2 flex items-center justify-center">
+         <div></div> upload!
+        </div>
+      </div>
     </div>
   );
 };
