a45d51c3d17aa55ca0cf135823fc18b22827f741
Fidelis Adewole <adewoyeadedayo10@gmail.com>
2024-09-23 07:51:01 +0100

    Update overviewCard.jsx

--- src/container/dashboard/home/overviewCard.jsx
+++ src/container/dashboard/home/overviewCard.jsx
@@ -1,7 +1,7 @@
 import { Card } from "@chakra-ui/react";
 import React from "react";
 
-const OverviewCard = ({ name, value, bg, text_color }) => {
+const OverviewCard = ({ name, percentage, value, bg, text_color }) => {
   return (
     <Card className="rounded-[8px] px-5 py-2 h-[85px]  lg:h-[140px] flex flex-col shadow-[2px_2px_4px_0_rgba(0,0,0,0.1)] border-[#0000004D]">
       <h3 className="text-[14px] lg:text-[20px] font-[500]">{name}</h3>
@@ -10,7 +10,7 @@ const OverviewCard = ({ name, value, bg, text_color }) => {
           style={{ backgroundColor: bg, color: text_color }}
           className="text-[8px] px-2 py-1 lg:py-2 lg:px-4 rounded-full"
         >
-          0% Increase
+          {percentage}% Increase
         </p>
 
         <p className="text-[20px] lg:text-[40px] ">{value}</p>
