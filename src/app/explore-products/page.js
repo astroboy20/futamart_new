ef1cac2c9bbfae86cb47057020fee5aaf6a931c3
astroboy20 <tolulopeakinkunmi7@gmail.com>
2024-10-21 09:21:24 +0100

    update

--- src/app/explore-products/page.js
+++ src/app/explore-products/page.js
@@ -1,12 +1,12 @@
 import { Footer } from "@/components/footer";
 import { Header } from "@/components/headers/header";
-import { ExploreComponent } from "@/container/home/products/pages/explore";
+// import { ExploreComponent } from "@/container/home/products/pages/explore";
 
 export default function Page() {
   return (
     <main>
       <Header />
-      <ExploreComponent />
+      {/* <ExploreComponent /> */}
       <Footer />
     </main>
   );
