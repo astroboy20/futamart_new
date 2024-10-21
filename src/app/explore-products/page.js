6a37f7d9007be3978545b1bacd316a692c69969d
astroboy20 <tolulopeakinkunmi7@gmail.com>
2024-10-21 08:48:03 +0100

    update

--- src/app/explore-products/page.js
+++ src/app/explore-products/page.js
@@ -1,6 +1,5 @@
 import { Footer } from "@/components/footer";
 import { Header } from "@/components/headers/header";
-import { ExploreComponent } from "@/container/home/products/exploreComponent";
 
 export default function Page() {
   return (
