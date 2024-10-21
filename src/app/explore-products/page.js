fff09d12edffb321ffabc90682c1138aa17e5d97
astroboy20 <tolulopeakinkunmi7@gmail.com>
2024-10-21 09:09:02 +0100

    update: fix import error

--- src/app/explore-products/page.js
+++ src/app/explore-products/page.js
@@ -1,11 +1,12 @@
 import { Footer } from "@/components/footer";
 import { Header } from "@/components/headers/header";
+import { ExploreComponent } from "@/container/home/products/pages/exploreComponent";
 
 export default function Page() {
   return (
     <main>
       <Header />
-      {/* <ExploreComponent /> */}
+      <ExploreComponent />
       <Footer />
     </main>
   );
