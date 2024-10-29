2c88b3d11b6ca29ec615d4e86756f11a468712b8
Fidel Wole <adewoyeadedayo10@gmail.com>
2024-10-29 19:15:51 +0100

    feat: get business logo in chat

--- src/container/user/chat/chatInput.jsx
+++ src/container/user/chat/chatInput.jsx
@@ -61,12 +61,14 @@ const ChatInput = ({
               </button>
             )}
             <Avatar>
-              {messages?.data?.userInfo?.profile_image ||
-              selectedUser?.userInfo?.profile_image ? (
-                <AvatarImage src={selectedUser.userInfo.profile_image} />
-              ) : (
-                <AvatarImage src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png" />
-              )}
+              
+                <AvatarImage
+                  src={
+                    messages?.data?.userInfo?.business_logo ||
+                    selectedUser?.businessInfo?.business_logo
+                  }
+                />
+              
               <AvatarFallback>
                 {messages?.data?.userInfo?.businessName ||
                   selectedUser?.businessInfo?.businessName}
