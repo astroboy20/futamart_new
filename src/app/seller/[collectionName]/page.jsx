b99f042a830128e00420ebae57870c5c8afb58f1
Fidel Wole <adewoyeadedayo10@gmail.com>
2024-10-29 09:38:42 +0100

    feat: meta data for seller page

--- src/app/seller/[collectionName]/page.jsx
+++ src/app/seller/[collectionName]/page.jsx
@@ -1,12 +1,20 @@
 import { Footer } from "@/components/footer";
 import SellerPage from "@/container/viewSeller/SellerPage"; 
 import { Header } from "@/components/headers/header";
+import Head from "next/head"; // Import the Head component
+
 export default async function SellerPageDynamic({ params }) {
   return (
     <div>
+      <Head>
+        <title>Seller Profile - futamart</title>
+        <meta name="description" content="Connect with thousands of buyers and boost your sales on futamart's seller platform!" /> 
+        <meta name="keywords" content="futamart, sell online, marketplace, seller page" /> 
+        <link rel="canonical" href={`https://futamart.vercel.app/seller/${params}`} /> 
+      </Head>
       <Header />
-      <SellerPage params={params} />;
-      <Footer/>
+      <SellerPage params={params} />
+      <Footer />
     </div>
   );
 }
