ae40cb6440234249fc8ae0c0080d9edf42a80e83
astroboy20 <tolulopeakinkunmi7@gmail.com>
2024-11-06 21:53:37 +0100

    clean-up

--- src/app/search/page.jsx
+++ src/app/search/page.jsx
@@ -1,15 +1,7 @@
 "use client";
 import { Header } from "@/components/headers/header";
-import { useParams, useSearchParams } from "next/navigation";
-import Link from "next/link";
-import { StarRating } from "@/components/rating";
-import { AddToCart } from "@/components/addToCart";
-import { FiChevronLeft, FiChevronRight } from "react-icons/fi";
+import { useSearchParams } from "next/navigation";
 import { Footer } from "@/components/footer";
-import { useState } from "react";
-import { BASE_URL, useFetchItems } from "@/hooks/useFetchItems";
-import { Loading } from "@/components/loading";
-import { AddToFavourite } from "@/components/AddToFavourite";
 import { Search } from "@/app/search/components/search";
 export default function Page() {
   const searchParams = useSearchParams();
