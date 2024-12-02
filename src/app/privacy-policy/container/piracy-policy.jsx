b98135331967a4218f1a1a32114d72a9610b7db3
astroboy20 <tolulopeakinkunmi7@gmail.com>
2024-12-02 05:30:17 +0100

    feat: add privacy-settings page

--- src/app/privacy-policy/container/piracy-policy.jsx
+++ src/app/privacy-policy/container/piracy-policy.jsx
@@ -3,84 +3,77 @@ import { Button } from "@/components/ui/button";
 import React, { useState } from "react";
 // import Image from "next/image";
 
-const Term = () => {
+const PiracyPolicy = () => {
   const [isChecked, setIsChecked] = useState(false);
-  const handleCheckChange = (e) => {
+  const handleCheckboxChange = (e) => {
     setIsChecked(e.target.checked);
   };
   return (
     <div className="flex flex-col gap-5  py-[3%] px-[6%]">
       <div className="w-full m-auto bg-[url('/images/overlay.png')] h-[400px] flex justify-center items-center">
         <h1 className="text-[30px] lg:text-[60px] font-[600] text-[white]">
-          Terms of Service
+          Privacy Settings
         </h1>
       </div>
       <div>
-        <ul className="text-[16p] lg:text-[20px] font-[400] list-disc flex flex-col gap-5 text-justify">
-          <li>
-            {" "}
+        <div className="text-[16p] lg:text-[20px] font-[400] list-disc flex flex-col gap-5 text-justify">
+          <p>
             Yorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc
             vulputate libero et velit interdum, ac aliquet odio mattis. Class
             aptent taciti sociosqu ad litora torquent per conubia nostra, per
             inceptos himenaeos. Yorem ipsum dolor sit amet, consectetur
             adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet
-            odio mattis.{" "}
-          </li>
-          <li>
+            odio mattis. Class aptent taciti sociosqu ad litora torquent per
+            conubia nostra, per inceptos himenaeos.Yorem ipsum dolor sit amet,
+            consectetur adipiscing elit. Nunc vulputate libero et velit
+            interdum, ac aliquet odio mattis. Class aptent taciti sociosqu ad
+            litora torquent per conubia nostra, per inceptos himenaeos.Yorem
+            ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate
+            libero et velit interdum, ac aliquet odio mattis. Class aptent
+            taciti sociosqu ad litora torquent per conubia nostra, per inceptos
+            himenaeos.vYorem ipsum dolor sit amet, consectetur adipiscing elit.
+            Nunc vulputate libero et velit interdum, ac aliquet odio mattis.
             Class aptent taciti sociosqu ad litora torquent per conubia nostra,
             per inceptos himenaeos.Yorem ipsum dolor sit amet, consectetur
             adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet
             odio mattis. Class aptent taciti sociosqu ad litora torquent per
             conubia nostra, per inceptos himenaeos.Yorem ipsum dolor sit amet,
             consectetur adipiscing elit. Nunc vulputate libero et velit
-            interdum, ac aliquet odio mattis.{" "}
-          </li>
-          <li>
-            Class aptent taciti sociosqu ad litora torquent per conubia nostra,
-            per inceptos himenaeos.vYorem ipsum dolor sit amet, consectetur
-            adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet
-            odio mattis. Class aptent taciti sociosqu ad litora torquent per
-            conubia nostra, per inceptos himenaeos.Yorem ipsum dolor sit amet,
-            consectetur adipiscing elit. Nunc vulputate libero et velit
-            interdum, ac aliquet odio mattis.{" "}
-          </li>
-          <li>
-            Class aptent taciti sociosqu ad litora torquent per conubia nostra,
-            per inceptos himenaeos.Yorem ipsum dolor sit amet, consectetur
-            adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet
-            odio mattis. Class aptent taciti sociosqu ad litora torquent per
-            conubia nostra, per inceptos himenaeos.Yorem ipsum dolor sit amet,
-            consectetur adipiscing elit.{" "}
-          </li>
-          <li>
+            interdum, ac aliquet odio mattis. Class aptent taciti sociosqu ad
+            litora torquent per conubia nostra, per inceptos himenaeos.Yorem
+            ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate
+            libero et velit interdum, ac aliquet odio mattis. Class aptent
+            taciti sociosqu ad litora torquent per conubia nostra, per inceptos
+            himenaeos.Yorem ipsum dolor sit amet, consectetur adipiscing elit.
+            Nunc vulputate libero et velit interdum, ac aliquet odio mattis.
             Class aptent taciti sociosqu ad litora torquent per conubia nostra,
             per inceptos himenaeos.Yorem ipsum dolor sit amet, consectetur
             adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet
             odio mattis. Class aptent taciti sociosqu ad litora torquent per
             conubia nostra, per inceptos himenaeos.Yorem ipsum dolor sit amet,
             consectetur adipiscing elit. Nunc vulputate libero et velit
-            interdum, ac aliquet odio mattis.
-          </li>
+            interdum, ac aliquet odio mattis. Class aptent taciti sociosqu ad
+            litora torquent per conubia nostra, per inceptos himenaeos.Yorem
+            ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate
+            libero et velit interdum, ac aliquet odio mattis.{" "}
+          </p>
 
           <label className="flex gap-2 items-center">
             <input
               type="checkbox"
               className="w-6 h-6"
               checked={isChecked}
-              onChange={handleCheckChange}
+              onChange={handleCheckboxChange}
             />{" "}
             <p>I agree to this conditions</p>
           </label>
-        </ul>
+        </div>
       </div>
-      <Button
-        disabled={!isChecked}
-        className="text-[16px] lg:text-[20px] font-[600] h-[55px]"
-      >
+      <Button disabled={!isChecked} className="text-[16px] lg:text-[20px] font-[600] h-[55px]">
         Agree
       </Button>
     </div>
   );
 };
 
-export { Term };
+export { PiracyPolicy };
